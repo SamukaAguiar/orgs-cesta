804e4155a102be2ef269df92ba60c004e2a0ab36
samukaaguiar <samuel@linqcode.com.br>
2022-12-09 19:42:17 -0300

    Atualizando projeto

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Cesta from './src/forms/Cesta';
+import MyComponent from './src/forms/MyComponent';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Cesta/>
+      <MyComponent/>
       <StatusBar style="auto" />
     </View>
   );
---
+++ src/forms/MyComponent.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Text } from "react-native";
+
+class MyComponent extends Component {
+  render() {
+    return <Text>Olá mundo!</Text>;
+  }
+}
+
+export default MyComponent;
+
+// Esta forma é pouco usada na comunidade 
